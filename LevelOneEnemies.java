78d40041e6a965029eb71e94717927aad3ba65a7
abbyejackson <jacks329@miamioh.edu>
2023-04-24 16:39:08 -0400

    Add files via upload

---
+++ LevelOneEnemies.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class LevelOneEnemies extends Enemies {
+
+    public LevelOneEnemies(int x, int y, int height, int width,
+            double enemySpeed) {
+        super(x, y, height, width, enemySpeed);
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ LevelTwoEnemies.java
@@ -0,0 +1,10 @@
+
+public class LevelTwoEnemies extends Enemies {
+
+    public LevelTwoEnemies(int x, int y, int height, int width,
+            double enemySpeed) {
+        super(x, y, height, width, enemySpeed);
+        // TODO Auto-generated constructor stub
+    }
+
+}
