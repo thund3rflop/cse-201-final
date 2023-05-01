627db5ad8707c4974308a57b69edeb0492357087
abbyejackson <jacks329@miamioh.edu>
2023-04-30 22:06:01 -0400

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -14,40 +14,108 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+/**
+* A class that creates the game panel.
+* 
+* @authors Chase Hollander, Abby Jackson, Sam Kujawa, Chanakya Pandya
+*/ 
 public class GamePanel extends JPanel implements MouseMotionListener {
     private static final long serialVersionUID = 1L;
+    /**
+    * Creates array list of enemies.
+    */ 
     private ArrayList<Enemies> enemies;
+    
+    /**
+    * Creates array list of projectiles.
+    */ 
     private ArrayList<Projectiles> projectiles; 
+    
+    /**
+    * Creates random generator.
+    */ 
     private Random random;
+    
+    /**
+    * Enemy's size. 
+    */ 
     private int enemySize;
-    private int enemyDeaths;
+    
+    /**
+    * Creates turret. 
+    */
     private Tank turret; 
+    
+    /**
+    * Creates hidden item. 
+    */
     private HiddenItem item; 
-    private int deathGoal = 15;
+    
+    /**
+    * Players can only have 25 deaths. 
+    */ 
+    private int deathGoal = 25;
+    
+    /**
+    * Player have to beat the level in 2 minutes for a bonus. 
+    */ 
     private long timeGoal = 120000;
+    
+    /**
+    * The base score in which players win the level. 
+    */ 
     private int scoreGoal = 1000;
+    
+    /** 
+    * Multiplier for hititng the hidden item. 
+    */ 
     private double itemMulti = 0.5;
+    
+    /** 
+    * Multiplier for beating time goal. 
+    */ 
     private double timeMulti = 0.75; 
+    
+    /**
+    * Total amount of deaths. 
+    */ 
     private static int deathCount = 0;
+    
+    /**
+    * Total score. 
+    */ 
     private static int score;
+    
+    /** 
+    * Whether or not hidden item was hit. 
+    */ 
     private static boolean hiddenHit; 
 
+    /**
+    * Creates the game panel.
+    */ 
     public GamePanel() {
+        // Creates mouse motion listener. 
         addMouseMotionListener(this);
+        // Initializes arrays. 
         enemies = new ArrayList<>();
         random = new Random();
         enemySize = 30; // Adjust this value for desired enemy size
         enemyDeaths = 0;
-         // Creates the hidden item.
+        // Creates the hidden item.
         int x = (int)(Math.random() * 800); 
         int y = (int)(Math.random() * 600); 
         item = new HiddenItem(x, y, 10, 10, 0.0); 
         
+        // Creates turret and sets backgrgound. 
         turret = new Tank(); 
         setPreferredSize(new Dimension(800, 600));
         setBackground(Color.BLACK);
     }
-    // Detects if enemies are hit by projectiles. 
+    
+    /**
+    * Detects if enemies are hit by projectiles. 
+    */
     public void detectCollision() {
         // Uses bounds for enemies and projectiles to detect intersection.
         for (int i = 0; i < enemies.size(); i++) {
@@ -64,11 +132,18 @@ public class GamePanel extends JPanel implements MouseMotionListener {
         }
     }
     
+    /**
+    * Gets the total score.
+    *
+    * @return The score. 
+    */ 
     public int getScore() {
         return this.score; 
     }
     
-    // Detects if enemies reach the turret. 
+    /**
+    * Detects if enemies reach the turret. 
+    */ 
     public void detectDeath() {
         // Uses bounds for enemies and turret to detect death. 
         for (int i = 0; i < enemies.size(); i++) {
@@ -81,13 +156,24 @@ public class GamePanel extends JPanel implements MouseMotionListener {
         }
     }   
     
+    /**
+    * Gets number of deaths.
+    * 
+    * @return The number of deaths. 
+    */
     public int getDeath() {
         return this.deathCount; 
     }
     
+    /**
+    * Detects if the hidden item was hit.
+    *
+    * @return A boolean stating if hidden item was hit by a projectile. 
+    */ 
     public boolean detectHiddenItem() {
         ArrayList<Enemies> hiddenItem = new ArrayList<>(); 
         hiddenItem.add(item); 
+        // Gets the bounds of the hidden item and all projectiles. 
         Rectangle hiddenRec = item.getBounds();
         for (int i = 0; i < projectiles.size(); i++) {
             Rectangle projectileHit = projectiles.get(i).getBounds();
@@ -101,24 +187,41 @@ public class GamePanel extends JPanel implements MouseMotionListener {
         return hiddenHit; 
     }
     
+    /**
+    * Gets if the hidden item was hit or not.
+    * 
+    * @return Whether or not the player hit the hidden item.
+    */ 
     public boolean getHit() {
         return hiddenHit; 
     }
 
+    /**
+    * Determines if mouse moved.
+    */ 
     public void mouseMoved() {
         
     }
+    
+    /**
+    * Overriden class that paints everything.
+    */ 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        // Paints the enemies. 
         for (Enemies enemy : enemies) {
             enemy.paint(g);
         }
+        // Paints the hidden item. 
         item.paintComponent(g); 
-        turret.paintComponent(g); // Paint the turret object
-
+        // Paints the turret object
+        turret.paintComponent(g); 
     }
 
+    /**
+    * Spawn enemies.
+    */ 
     public void spawnEnemies() {
         if (random.nextInt(100) < .5) { // Adjust this value for desired spawn rate
             int x, y;
@@ -142,6 +245,9 @@ public class GamePanel extends JPanel implements MouseMotionListener {
         }
     }
 
+    /**
+    * Move the enemies.
+    */ 
     public void moveEnemies() {
         double centerX = getWidth() / 2.0;
         double centerY = getHeight() / 2.0;
@@ -152,19 +258,16 @@ public class GamePanel extends JPanel implements MouseMotionListener {
             double distance = Math.sqrt(dx * dx + dy * dy);
             double moveX = dx * enemy.getEnemySpeed() / distance;
             double moveY = dy * enemy.getEnemySpeed() / distance;
-
             if (enemy instanceof CustomEnemy) {
                 ((CustomEnemy) enemy).move(moveX, moveY);
-
             }
-
         }
     }
 
+    // Overriden methods for the mouse motion listener. 
     @Override
     public void mouseDragged(MouseEvent e) {
         // TODO Auto-generated method stub
-        
     }
 
     @Override
