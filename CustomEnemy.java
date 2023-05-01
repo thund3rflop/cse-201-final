640cfe5667a817674e82a763575a4549d92ba471
abbyejackson <jacks329@miamioh.edu>
2023-04-30 21:44:28 -0400

    Update CustomEnemy.java

--- CustomEnemy.java
+++ CustomEnemy.java
@@ -12,6 +12,14 @@ import javax.imageio.ImageIO;
 public class CustomEnemy extends Enemies {
     private Image enemyImage;
 
+    /**
+    * Creates a custom enemy with an image.
+    * @param x The x-position. 
+    * @param y The y-position. 
+    * @param height The hieght of the enemy. 
+    * @param width The width of the enemy.
+    * @param enemySpeed The enemy's speed.
+    */ 
     public CustomEnemy(int x, int y, int height, int width, double enemySpeed) {
         super(x, y, height, width, enemySpeed);
         try {
@@ -21,6 +29,11 @@ public class CustomEnemy extends Enemies {
         }
     }
 
+    /**
+    * Moves the enemies towards the center.
+    * @param moveX Moves the enemy along x-axis.
+    * @param moveY Moves the enemy along the y-axis.
+    */
     public void move(double moveX, double moveY) {
         int newX = (int) (getBounds().x + moveX);
         int newY = (int) (getBounds().y + moveY);
@@ -28,6 +41,7 @@ public class CustomEnemy extends Enemies {
         repaint();
     }
 
+    // Overriden method that paints the custom enemy with an image. 
     @Override
     public void paint(Graphics g) {
         if (enemyImage != null) {
